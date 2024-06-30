4d0af261d6f427fb524521820e52590f768675a3
Rodrigo Batista <57587866+robatist@users.noreply.github.com>
2024-06-30 14:44:17 +0100

    Common integration (#1)
    * evol: parent pom changed to the common project
    
    * evol: parent pom changed to the common project

--- java/src/main/java/com/robatist/exports/back/KafkaConsumerApplication.java
+++ java/src/main/java/com/robatist/exports/back/KafkaConsumerApplication.java
@@ -1,4 +1,4 @@
-package com.robatist.backend;
+package com.robatist.exports.back;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
